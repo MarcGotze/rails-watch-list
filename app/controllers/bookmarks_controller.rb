d580993c98dfa83b980a1f9bba629772b10b7d26
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-17 21:03:20 +0200

    WIP

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,27 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = @list.bookmarks.new
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
