6d5a29a7888cef7ae29f43e5f514cf36e62ceca4
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-17 15:44:27 +0200

    Routes defined

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+require 'json'
+
+Movie.destroy_all
+URL = "https://tmdb.lewagon.com/movie/top_rated"
+uri = URI(URL)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)['results']
+
+puts "Creating movies..."
+puts "========================================"
+data.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    rating: movie['vote_average']
+  )
+  puts movie['title']
+end
+puts "========================================"
+puts "Completed !!"
